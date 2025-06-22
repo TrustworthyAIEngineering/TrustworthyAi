ad4a6f43202e755b980bb8ec32b54357361096a4
Bruce Bi <zhaoge.bi@sydney.edu.au>
2025-06-23 08:16:42 +1000

    Setted title for news page

--- frontend/src/pages/News.js
+++ frontend/src/pages/News.js
@@ -1,10 +1,16 @@
 import React from "react";
-
+import "../styles/News.css"
 function News(){
     return(
         <>
             <div id="whole" className="d-flex flex-column justify-content-start align-items-center mt-lg-5 min-vh-100 w-100">
-                Coming soon
+                <div className="news-container">
+                    <h1 className="news-title">News Posts</h1>
+                    <p className="news-subtitle">Catch our latest updates from our team</p>
+                    <div className="news-underline" />
+                    Coming soon
+
+                </div>
             </div>
         </>
     )
